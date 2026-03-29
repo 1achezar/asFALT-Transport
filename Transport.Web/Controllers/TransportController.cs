9b8fdf22bb18c37a826e35e697d8ca89bea794cd
tdtomov <tdtomov@gmail.com>
2026-03-29 09:02:16 +0200

    created new pages for each line

---
+++ Transport.Web/Controllers/TransportController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Transport.Services;
+
+namespace Transport.Web.Controllers
+{
+    public class TransportController : Controller
+    {
+        private readonly ITransportService _transportService;
+
+        public TransportController(ITransportService transportService)
+        {
+            _transportService = transportService;
+        }
+
+        // Route-based: /Transport/Stops/{line}
+        [Route("Transport/Stops/{line}")]
+        public IActionResult Stops(string line)
+        {
+            var stops = _transportService.GetStopsForLine(line);
+            ViewBag.Line = line;
+
+            return View(stops);
+        }
+    }
+}
